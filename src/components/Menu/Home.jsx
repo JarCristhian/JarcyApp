a313cab0400e0429820abaa8be865ecae4e1cc24
Jar Cristhian <jacrixx@gmail.com>
2023-01-03 23:00:43 -0500

    add pdf

--- src/components/Menu/Home.jsx
+++ src/components/Menu/Home.jsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import logo from "../../img/logo.png";
+import cv from "../../hook/download/cv.pdf";
 
 const NavBar = () => {
   return (
@@ -21,7 +22,11 @@ const NavBar = () => {
               increíbles para el usuario. Actualmente enfocado en el desarrollo Web y Móvil.
             </p>
             <div className="flex justify-center ml-2">
-              <button className="inline-flex text-white bg-green-400 border-0 py-2 px-6 focus:outline-none hover:bg-green-500 rounded text-lg">
+              <a
+                href={cv}
+                download="jarcyCV"
+                className="inline-flex text-white bg-green-400 border-0 py-2 px-6 focus:outline-none hover:bg-green-500 rounded text-lg"
+              >
                 Resumen
                 <svg className="ml-1 mt-1" width="22" height="22" viewBox="0 0 24 24">
                   <path
@@ -29,7 +34,7 @@ const NavBar = () => {
                     d="M12 15.575q-.2 0-.375-.063q-.175-.062-.325-.212l-3.6-3.6q-.275-.275-.275-.7q0-.425.275-.7q.275-.275.712-.288q.438-.012.713.263L11 12.15V5q0-.425.288-.713Q11.575 4 12 4t.713.287Q13 4.575 13 5v7.15l1.875-1.875q.275-.275.713-.263q.437.013.712.288q.275.275.275.7q0 .425-.275.7l-3.6 3.6q-.15.15-.325.212q-.175.063-.375.063ZM6 20q-.825 0-1.412-.587Q4 18.825 4 18v-2q0-.425.287-.713Q4.575 15 5 15t.713.287Q6 15.575 6 16v2h12v-2q0-.425.288-.713Q18.575 15 19 15t.712.287Q20 15.575 20 16v2q0 .825-.587 1.413Q18.825 20 18 20Z"
                   />
                 </svg>
-              </button>
+              </a>
               <a
                 href="#contact"
                 draggable={false}
